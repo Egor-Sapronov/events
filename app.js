08241aefb178a9436fc7fa79f42a1a07d0378cc7
Egor <sapronov.egor@gmail.com>
2015-02-01 20:15:51 +0600

    style(cleanup)

--- app.js
+++ app.js
@@ -2,13 +2,9 @@
 
 var express = require('express'),
     auth = require('./libs/auth/auth'),
-    bodyParser = require('body-parser'),
     passport = require('passport'),
     app = express();
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
 
 app.get('/', function (req, res) {
