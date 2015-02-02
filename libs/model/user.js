9c44d28a78d428a7506f89862bbfb854c2286a9c
Egor <sapronov.egor@gmail.com>
2015-02-02 22:41:14 +0600

    feat(validate): username, password, email

--- libs/model/user.js
+++ libs/model/user.js
@@ -17,15 +17,27 @@ module.exports = function (sequelize, DataTypes) {
             username: {
                 type: DataTypes.STRING,
                 unique: true,
-                allowNull: false
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                    is: /[a-z0-9A-Z-]{4,30}/
+                }
             },
-            email:{
+            email: {
                 type: DataTypes.STRING,
                 unique: true,
-                allowNull: false
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                    isEmail: true
+                }
             },
             password: {
                 type: DataTypes.STRING,
+                validate: {
+                    notEmpty: true,
+                    is: /[a-z0-9A-Z-]{6,30}/
+                },
                 get: function () {
                     return this._plainPassword;
                 },
