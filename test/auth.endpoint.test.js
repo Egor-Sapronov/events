4561c5f8c298d8da16f8a8073c9492c12a0f108b
Egor <sapronov.egor@gmail.com>
2015-02-09 21:25:35 +0600

    test(/auth/login): test login endpoint with supertest

---
+++ test/auth.endpoint.test.js
@@ -0,0 +1,39 @@
+var request = require('supertest'),
+    app = require('../app'),
+    db = require('../libs/data/database');
+
+describe('Auth endpoints', function () {
+    describe('#/auth/login', function () {
+        it('Should exchange token for email and password', function (done) {
+            db.sequelize.sync({force: true})
+                .then(function () {
+                    return db.User.create({
+                        username: 'egor',
+                        password: '123456',
+                        email: 'sapronov.egor@gmail.com'
+                    });
+                })
+                .then(function () {
+                    var encodedCredential = new Buffer('sapronov.egor@gmail.com:123456').toString('base64');
+
+                    function hasToken(res) {
+                        if (!('token' in res.body))
+                            return 'missing access token';
+                    }
+
+                    function hasUsername(res) {
+                        if (!('username' in res.body))
+                            return 'missing username';
+                    }
+
+                    request(app)
+                        .get('/auth/login')
+                        .set('Authorization', 'Basic ' + encodedCredential)
+                        .expect(hasToken)
+                        .expect(hasUsername)
+                        .expect(200, done);
+                });
+        });
+    });
+});
+
