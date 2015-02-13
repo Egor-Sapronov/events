5afbed954d9f0143cf7cf59ea30d61bb46d4d68b
Egor <sapronov.egor@gmail.com>
2015-02-13 22:25:37 +0600

    feat(role): define role model

---
+++ libs/model/role.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+    return sequelize.define('Role', {
+        title: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isAlphanumeric: true
+            }
+        }
+    });
+};
