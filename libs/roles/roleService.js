762f53049a83a05ec7d4e6033f06328a86677cc0
Egor <sapronov.egor@gmail.com>
2015-02-14 12:08:53 +0600

    fix(roleHandler): return 500 status code if user is not in request
    It's api error if we have endpoint with role handler but without user in request

--- libs/roles/roleService.js
+++ libs/roles/roleService.js
@@ -13,13 +13,38 @@ function inRole(roleTitle, user) {
     return db.Role
         .find({where: {title: roleTitle}})
         .then(function (role) {
-            if (!role)
+            if (!role) {
                 return false;
+            }
 
             return user.hasRole(role);
         });
 }
 
+/**
+ * Check that user in request has role
+ *
+ * @param roleTitle
+ * @returns {Function}
+ */
+function roleHandler(roleTitle) {
+    return function (req, res, next) {
+        if (!req.user) {
+            res.status(500).end();
+        }
+
+        inRole(roleTitle, req.user)
+            .then(function (result) {
+                if (result) {
+                    next();
+                }
+
+                res.status(401).end();
+            });
+    };
+}
+
 module.exports = {
-    inRole: inRole
+    inRole: inRole,
+    roleHandler: roleHandler
 };
