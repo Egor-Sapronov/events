aaf7b8e47464e3aade8ef2318923719cd54d92b2
Egor <sapronov.egor@gmail.com>
2015-02-16 22:22:22 +0600

    feat(subscribeEvent): subscribe user for the event

--- libs/events/eventService.js
+++ libs/events/eventService.js
@@ -2,14 +2,38 @@
 
 var db = require('../data/database');
 
+/**
+ * Create event for the user, who become event owner
+ *
+ * @param user event owner
+ * @param eventData
+ * @returns {Bluebird.Promise|*}
+ */
 function createEvent(user, eventData) {
     return db.Event.create(eventData)
         .then(function (event) {
             return event.setOwner(user);
         });
+}
 
+/**
+ * Subscribe user for the event
+ *
+ * @param user event subscriber
+ * @param eventId
+ * @returns {Bluebird.Promise|*}
+ */
+function subscribeEvent(user, eventId) {
+    return db.Event.find({
+        where: {
+            id: eventId
+        }
+    }).then(function (event) {
+        return event.addUser(user);
+    });
 }
 
 module.exports = {
-    createEvent: createEvent
+    createEvent: createEvent,
+    subscribeEvent: subscribeEvent
 };
--- test/UNIT/event.service.test.js
+++ test/UNIT/event.service.test.js
@@ -51,4 +51,84 @@ describe('Event service', function () {
                 });
         });
     });
+
+    describe('#subscribeEvent', function () {
+        it('Should subscribe user to event', function (done) {
+            var subscriber, event, owner;
+            db.sequelize.sync({force: true})
+                .then(function () {
+                    return db.User.create({
+                        username: 'egor',
+                        password: '123456',
+                        email: 'sapronov.egor@gmail.com'
+                    });
+                })
+                .then(function (entity) {
+                    owner = entity;
+                    return eventService.createEvent(owner, {
+                        title: 'test',
+                        description: 'Test description'
+                    });
+                })
+                .then(function (entity) {
+                    event = entity;
+                    return db.User.create({
+                        username: 'egor2',
+                        password: '123456',
+                        email: 'sapronov2.egor@gmail.com'
+                    });
+                })
+                .then(function (entity) {
+                    subscriber = entity;
+                    return eventService.subscribeEvent(subscriber, event.id);
+                })
+                .then(function (entity) {
+                    expect(entity).to.be.ok;
+                    return event.hasUser(subscriber);
+                })
+                .then(function (result) {
+                    expect(result).to.be.ok;
+                    return event.getOwner();
+                })
+                .then(function (result) {
+                    expect(result.id).to.equal(owner.id);
+                    done();
+                })
+        });
+
+        it('Should throw error if event validation fail', function (done) {
+            var subscriber, event, owner;
+            db.sequelize.sync({force: true})
+                .then(function () {
+                    return db.User.create({
+                        username: 'egor',
+                        password: '123456',
+                        email: 'sapronov.egor@gmail.com'
+                    });
+                })
+                .then(function (entity) {
+                    owner = entity;
+                    return eventService.createEvent(owner, {
+                        title: 'test',
+                        description: 'Test description'
+                    });
+                })
+                .then(function (entity) {
+                    event = entity;
+                    return db.User.create({
+                        username: 'egor2',
+                        password: '123456',
+                        email: 'sapronov2.egor@gmail.com'
+                    });
+                })
+                .then(function (entity) {
+                    subscriber = entity;
+                    return eventService.subscribeEvent(subscriber, 'bad id');
+                })
+                .catch(function (err) {
+                    expect(err).to.be.ok;
+                    done();
+                });
+        });
+    });
 });
