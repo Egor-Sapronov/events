d508752d30b6e3989942d94c9af0d1cc315b7369
Egor <sapronov.egor@gmail.com>
2015-03-07 12:59:56 +0600

    fix(app): delete empty module reference

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express'),
     passport = require('./libs/auth/auth').passport,
-    authRouter = require('./routes/auth'),
     app = express();
 
 app.use(passport.initialize());
