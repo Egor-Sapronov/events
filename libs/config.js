decf24ed8ad2788276b92532a760bfbf33279627
Egor <sapronov.egor@gmail.com>
2015-03-07 17:30:26 +0600

    ignore env variables

--- libs/config.js
+++ libs/config.js
@@ -3,20 +3,23 @@ nconf.argv()
     .env()
     .file({file: 'config.json'});
 
+/* istanbul ignore if  */
 if (process.env.DATABASE_URL) {
     nconf.set('db:url', process.env.DATABASE_URL);
 }
-
+/* istanbul ignore if  */
 if (process.env.PORT) {
     nconf.set('app:port', process.env.PORT);
 }
-
+/* istanbul ignore if  */
 if (process.env.CLIENT_ID) {
     nconf.set('facebook:clientID', process.env.CLIENT_ID);
 }
+/* istanbul ignore if  */
 if (process.env.CLIENT_SECRET) {
     nconf.set('facebook:clientSecret', process.env.CLIENT_SECRET);
 }
+/* istanbul ignore if  */
 if (process.env.CALLBACK_URL) {
     nconf.set('facebook:callbackURL', process.env.CALLBACK_URL);
 }
