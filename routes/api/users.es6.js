9fa7af97d940d94271d519408c05adebee513445
Egor <sapronov.egor@gmail.com>
2015-03-11 14:44:29 +0600

    feat(api/user/:id): return user by id

--- routes/api/users.es6.js
+++ routes/api/users.es6.js
@@ -2,13 +2,17 @@
 
 let router = require('express').Router();
 let passport = require('../../libs/auth/auth.es6').passport;
+let userService = require('../../libs/userService.es6');
 
 router.get('/me', passport.authenticate('bearer', {sessions: false}), function (req, res) {
     res.send(req.user);
 });
 
 router.get('/:id', function (req, res) {
-
+    userService.getUser(req.params.id)
+        .then(function (user) {
+            res.send(user);
+        });
 });
 
 module.exports = router;
