def15ab37430b1b9ca7d4735a4ff4b1cdf239383
Egor <sapronov.egor@gmail.com>
2015-03-13 19:49:39 +0600

    fix(profileBar): react inline style

--- web/src/app/components/profileBar.react.jsx
+++ web/src/app/components/profileBar.react.jsx
@@ -2,9 +2,12 @@
 
 var ProfileBar = React.createClass({
     render: function () {
-        return <li style="height:100%;" className="valign-wrapper">
+        var liStyle = {
+            height: '100%'
+        };
+        return <li style={liStyle} className="valign-wrapper">
             <a href="#">
-                <img src="{this.props.imageSrc}" className="z-depth-1 responsive-img circle valign" />
+                <img src={this.props.imageSrc} className="z-depth-1 responsive-img circle valign" />
             </a>
         </li>;
     }
