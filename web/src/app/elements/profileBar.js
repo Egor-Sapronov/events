ff3fa3f11232ff2ba351de2af8f13953a6630797
Egor <sapronov.egor@gmail.com>
2015-03-13 19:43:04 +0600

    fix

--- web/src/app/elements/profileBar.js
+++ web/src/app/elements/profileBar.js
@@ -4,7 +4,7 @@ var userContext = require('../utils/dataContext').userContext;
 var profileBar = require('../components/profileBar.react.jsx');
 
 userContext.on('load::user', function () {
-    React.render(React.createElement(profileBar, {imageSrc: userContext.profileImage.url}, document.getElementById('profile-container')));
+    React.render(React.createElement(profileBar, {imageSrc: userContext.profileImage.url}), document.getElementById('profile-container'));
 });
 
 function init() {
