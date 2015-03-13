e5c21a46e3e05a6f2626363e74929ba233905812
Egor <sapronov.egor@gmail.com>
2015-03-13 19:24:46 +0600

    fix: hide hardcoded html

--- web/src/app/elements/profileBar.js
+++ web/src/app/elements/profileBar.js
@@ -4,7 +4,6 @@ var userContext = require('../utils/dataContext').userContext;
 var profileBar = require('../components/profileBar.react.jsx');
 
 userContext.on('load::user', function () {
-    console.table(userContext.user);
     React.render(profileBar, {imageSrc: userContext.profileImage.url}, document.getElementById('profile-cotainer'));
 });
 
