00f47959c5869321f0f669414d19e2263c825607
Egor <sapronov.egor@gmail.com>
2015-03-14 12:29:07 +0600

    fix(index)

--- web/src/app/index.js
+++ web/src/app/index.js
@@ -5,16 +5,12 @@ var profileBar = require('./elements/profileBar');
 $(document).ready(function () {
     if (location.hash.split('#')[1]) {
         userContext.saveToken(location.hash.split('#')[1]);
+        location.hash = '';
         profileBar.init();
     } else {
         if (userContext.getToken()) {
             profileBar.init();
         }
     }
-    //
-    //$(".side-activator").sideNav({
-    //    edge: 'right',
-    //    closeOnClick: true
-    //});
 });
 
