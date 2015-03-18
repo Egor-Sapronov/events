4374cd1a51344e6d096a5a009d380674e76074aa
Egor <sapronov.egor@gmail.com>
2015-03-18 22:30:17 +0600

    refactor(userBar): remove profileBar element
    Thir implimentation of UI elements break modules inpependents

--- web/src/app/create.js
+++ web/src/app/create.js
@@ -6,6 +6,8 @@ var descriptionDomNode = document.getElementById('description');
 var imageDomNode = document.getElementById('image');
 var dateDomNode = document.getElementById('eventDate');
 var placeDomNode = document.getElementById('place');
+var userContext = require('./utils/dataContext').userContext;
+var profileBar = require('./components/profileBar.react.jsx');
 var mountedPreview = React.render(React.createElement(previewCard, {
     image: 'http://materializecss.com/images/office.jpg',
     title: 'Event title',
@@ -13,6 +15,17 @@ var mountedPreview = React.render(React.createElement(previewCard, {
     date: 'Jan 9, 2014',
     description: 'Footers are a great way to organize a lot of site navigation and information at the end of a page.  This is where the user will look once hes finished scrolling through the current page or is looking for additional.'
 }), document.getElementById('preview-container'));
+
+userContext.on('load::user', function () {
+    React.render(React.createElement(
+            profileBar,
+            {imageSrc: userContext.profileImage.url}
+        ),
+        document.getElementById('profile-container'
+        ));
+});
+
+userContext.loadUserInfo();
 
 titleDomNode.oninput = function () {
     mountedPreview.setProps({
--- web/src/app/index.js
+++ web/src/app/index.js
@@ -1,16 +1,18 @@
 'use strict';
+
 var userContext = require('./utils/dataContext').userContext;
-var profileBar = require('./elements/profileBar');
+var profileBar = require('./components/profileBar.react.jsx');
+
+userContext.on('load::user', function () {
+    React.render(React.createElement(profileBar, {imageSrc: userContext.profileImage.url}), document.getElementById('profile-container'));
+});
 
 $(document).ready(function () {
     if (location.hash.split('#')[1]) {
         userContext.saveToken(location.hash.split('#')[1]);
         location.hash = '';
-        profileBar.init();
-    } else {
-        if (userContext.getToken()) {
-            profileBar.init();
-        }
     }
+
+    userContext.loadUserInfo();
 });
 
