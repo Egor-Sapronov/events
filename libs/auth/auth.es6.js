abbe90c5a859108889633c11ba2dda0d63cb4864
Egor <sapronov.egor@gmail.com>
2015-03-21 15:09:01 +0600

    fix(session): store user id

--- libs/auth/auth.es6.js
+++ libs/auth/auth.es6.js
@@ -7,32 +7,22 @@ let FacebookStrategy = require('passport-facebook');
 let config = require('../config.es6');
 let db = require('../data/database.es6');
 
-//passport.serializeUser(function (user, done) {
-//    console.log(user);
-//    done(null, user.id);
-//});
-//passport.deserializeUser(function (id, done) {
-//    console.log(id);
-//    db.User.find({
-//        where: {
-//            id: id
-//        }
-//    })
-//        .then(function (user) {
-//            console.log(user);
-//            done(null, user);
-//        });
-//
-//});
-
 passport.serializeUser(function (user, done) {
     console.log(user);
-    done(null, user);
+    done(null, user.id);
 });
+passport.deserializeUser(function (id, done) {
+    console.log(id);
+    db.User.find({
+        where: {
+            id: id
+        }
+    })
+        .then(function (user) {
+            console.log(user);
+            done(null, user);
+        });
 
-passport.deserializeUser(function (obj, done) {
-    console.log(obj);
-    done(null, obj);
 });
 
 passport.use(new BearerStrategy(strategy.bearerStrategy));
