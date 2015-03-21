a2cdcbb6bdbb587086ba0b8ac38486fc385cc09d
Egor <sapronov.egor@gmail.com>
2015-03-21 13:52:39 +0600

    fix(serialize user): use users from db

--- libs/auth/auth.es6.js
+++ libs/auth/auth.es6.js
@@ -5,12 +5,24 @@ let BearerStrategy = require('passport-http-bearer').Strategy;
 let strategy = require('./strategy.es6');
 let FacebookStrategy = require('passport-facebook');
 let config = require('../config.es6');
+let db = require('../data/database.es6');
 
 passport.serializeUser(function (user, done) {
-    done(null, user);
+    console.log(user);
+    done(null, user.id);
 });
-passport.deserializeUser(function (obj, done) {
-    done(null, obj);
+passport.deserializeUser(function (id, done) {
+    console.log(id);
+    db.User.find({
+        where: {
+            id: id
+        }
+    })
+        .then(function (user) {
+            console.log(user);
+            done(null, user);
+        });
+
 });
 
 passport.use(new BearerStrategy(strategy.bearerStrategy));
