2ab7974feb163e84c006ce083577222d9f3e5cba
Egor <sapronov.egor@gmail.com>
2015-03-21 19:12:28 +0600

    fix

--- web/src/app/utils/coreMediator.js
+++ web/src/app/utils/coreMediator.js
@@ -5,11 +5,11 @@ var userContext = require('./users/userContext');
 var profileBar = require('./../components/profileBar.react.jsx');
 
 userService.on('auth::success', function (user) {
-    userContext.setUser(user);
+    userContext.user = user;
 });
 
 userContext.on('user::update', function () {
-    var user = userContext.getUser();
+    var user = userContext.user;
 
     React.render(
         React.createElement(
