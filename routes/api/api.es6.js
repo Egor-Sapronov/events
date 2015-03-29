3c35a98286e7d63fc3ea8e1a1506e10336380874
Egor <sapronov.egor@gmail.com>
2015-03-29 13:19:29 +0600

    fix(api): setup api in 1 file

--- routes/api/api.es6.js
+++ routes/api/api.es6.js
@@ -1,8 +1,38 @@
 'use strict';
 
 let router = require('express').Router();
-let users = require('./users.es6');
+let passport = require('../../libs/auth/auth.es6').passport;
+let userService = require('../../libs/userService.es6');
 
-router.use('/users', users);
+router.get('/users/me', passport.authenticate('bearer', {session: false}), function (req, res) {
+    res.send({
+        displayName: req.user.displayName,
+        name: req.user.name,
+        id: req.user.id,
+        providerId: req.user.providerId,
+        gender: req.user.gender
+    });
+});
+
+router.post('/users/:id/events', passport.authenticate('bearer', {session: false}), function (req, res) {
+    res.send(req.body);
+});
+
+router.get('/users/:id', function (req, res) {
+    userService.getUser(req.params.id)
+        .then(function (user) {
+            if (user) {
+                res.send({
+                    displayName: user.displayName,
+                    name: user.name,
+                    id: user.id,
+                    providerId: user.providerId,
+                    gender: user.gender
+                });
+            } else {
+                res.status(404).end();
+            }
+        });
+});
 
 module.exports = router;
