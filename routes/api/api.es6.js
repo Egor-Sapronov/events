a492e9065bde734bc5eabb234729aecf292ce7f8
Egor <sapronov.egor@gmail.com>
2015-03-29 16:21:18 +0600

    feat(user id)

--- routes/api/api.es6.js
+++ routes/api/api.es6.js
@@ -5,8 +5,23 @@ let passport = require('../../libs/auth/auth.es6').passport;
 let userService = require('../../libs/userService.es6');
 let eventService = require('../../libs/events/eventService.es6');
 
-router.post('/users/:id/events', passport.authenticate('bearer', {session: false}), function (req, res) {
-    eventService.createEvent(req.user, req.body)
+router.all('*', function (req, res, next) {
+    req.context = {};
+    next();
+});
+
+router.param('user', function (req, res, next, id) {
+    userService.getUser(id)
+        .then(function (user) {
+            req.context.user = user;
+        })
+        .catch(function (err) {
+            res.status(400).end();
+        });
+});
+
+router.post('/users/:user/events', passport.authenticate('bearer', {session: false}), function (req, res) {
+    eventService.createEvent(req.context.user, req.body)
         .then(function (event) {
             res.status(201).send({
                 title: event.title,
@@ -20,11 +35,8 @@ router.post('/users/:id/events', passport.authenticate('bearer', {session: false
         });
 });
 
-router.get('/users/:id/events', function (req, res) {
-    userService.getUser(req.params.id)
-        .then(function (user) {
-            return eventService.getCreatedEvents(user);
-        })
+router.get('/users/:user/events', function (req, res) {
+    eventService.getCreatedEvents(req.context.user)
         .then(function (events) {
             res.status(200).send(events);
         })
@@ -43,21 +55,14 @@ router.get('/users/me', passport.authenticate('bearer', {session: false}), funct
     });
 });
 
-router.get('/users/:id', function (req, res) {
-    userService.getUser(req.params.id)
-        .then(function (user) {
-            if (user) {
-                res.send({
-                    displayName: user.displayName,
-                    name: user.name,
-                    id: user.id,
-                    providerId: user.providerId,
-                    gender: user.gender
-                });
-            } else {
-                res.status(404).end();
-            }
-        });
+router.get('/users/:user', function (req, res) {
+    res.send({
+        displayName: req.context.user.displayName,
+        name: req.context.user.name,
+        id: req.context.user.id,
+        providerId: req.context.user.providerId,
+        gender: req.context.user.gender
+    });
 });
 
 module.exports = router;
