03811f1fc16fe1fd3b7cd4d429a161b5e4f2b4b6
Egor <sapronov.egor@gmail.com>
2015-04-11 15:52:00 +0600

    fix

--- web/src/app/create.js
+++ web/src/app/create.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
         var file = fileInput.files[0];
         form.append('file', file);
 
-        fetch(decodeURI(data._metadata.image.signed_request), {
+        fetch(data._metadata.image.signed_request, {
             mode: 'cors',
             method: 'PUT',
             body: form,
